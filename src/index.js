0675fa459bfab96671c9dde53efdadb334d9aca3
Kristoffer Bråthen Halden <>
2020-03-13 16:14:49 +0100

    Restart, misunderstood the task

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// styles
-import './css/index.css';
-// componentns
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
