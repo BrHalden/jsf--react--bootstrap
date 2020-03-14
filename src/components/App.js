9e8b4e62ef514ee13d3de7c48729284401d77af3
Kristoffer Bråthen Halden <>
2020-03-14 10:42:42 +0100

    Error with index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Nav from "react-bootstrap/Nav";
+import NavBar from "react-bootstrap/NavBar";
+import Navbar from 'react-bootstrap/NavBar';
 
 export default function App() {
     return (
         <div className="App">
-            <nav>
-                <a href="/">Home</a>
-                <a href="/section1/">Section 1</a>
-                <a href="/section2/">Section 2</a>
-            </nav>
+            <NavBar bg="pimary" variant="dark" expand="lg">
+                <NavBar.Brand href="/">Website Name</NavBar.Brand>
+                <Navbar.Toggle aria-controls="collapse" />
+                <Navbar.Collapse react-html-id="collapse">
+                    <Nav>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/section1/">Section 1</Nav.Link>
+                        <Nav.Link href="/section2/">Section 2</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </NavBar>
         </div>
     );
 }
